c99171c85b6381f94bd2398f7b9aaefbdfcaa03a
Duncan Jacobsen <jacobsenduncan@gmail.com>
2021-03-05 18:54:08 -0700

    fixed more style bugs

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 
 import Header from './Header.jsx'
+import './styles/about.css'
+import Duncan from './images/duncan.jpg'
+import Tom from './images/tom.jpg'
 
 const About = () => {
     return(
         <div className="page-template-grid">
             <Header />
             <div className="content-template single-container">
-                <p>I'm an about page</p>
+                <img alt="Duncan in the desert" className="mug-shot" src={Duncan}/>
+                <img alt="zooTom skiing" className="mug-shot" src={Tom} />
             </div>
         </div>
     )
